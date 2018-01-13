aaee567e798f38a3bd518c7b93c45e0343c5dbc9
Monzil98 <manjilnepali98@gmail.com>
2018-01-13 13:18:16 +0545

    sign up

--- login_page/login_script.js
+++ login_page/login_script.js
@@ -1,1 +1,25 @@
-\
+$(document).ready(function(){
+	console.log("document is ready");
+
+	$('.signup__option>.btn').click(function(){
+		var tab_id=$(this).attr('data-tab');
+		
+		if(this.className!="btn btn--active"){
+			console.log("true");
+	   //      if(tab_id=='teacher'){
+	   //      	 $('.signup').css("height","93%");
+				// }else{
+				// 	 $('.signup').css("height","80%");
+				// }
+
+		$('.signup__option>.btn').removeClass('btn--active');
+		$('.signup__form').removeClass('signup__form--open');
+		$(this).addClass('btn--active');
+		$('#'+tab_id).addClass('signup__form--open');
+	        
+	}
+	})
+
+
+	
+});

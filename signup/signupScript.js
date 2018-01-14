d595ee771b4e67e1c91d35f85fbb768feb83025a
Monzil98 <manjilnepali98@gmail.com>
2018-01-14 19:38:56 +0545

    adding front end form data validation using jquery

--- signup/signupScript.js
+++ signup/signupScript.js
@@ -1,25 +1,37 @@
 $(document).ready(function(){
-	console.log("document is ready");
+console.log("document is ready");
+$('.signup__option>.btn').click(function(){
+var tab_id=$(this).attr('data-tab');
 
-	$('.signup__option>.btn').click(function(){
-		var tab_id=$(this).attr('data-tab');
-		
-		if(this.className!="btn btn--active"){
-			console.log("true");
-	   //      if(tab_id=='teacher'){
-	   //      	 $('.signup').css("height","93%");
-				// }else{
-				// 	 $('.signup').css("height","80%");
-				// }
+if(this.className!="btn btn--active"){
+console.log("true");
 
-		$('.signup__option>.btn').removeClass('btn--active');
-		$('.signup__form').removeClass('signup__form--open');
-		$(this).addClass('btn--active');
-		$('#'+tab_id).addClass('signup__form--open');
-	        
-	}
-	})
+$('.signup__option>.btn').removeClass('btn--active');
+$('.signup__form').removeClass('signup__form--open');
+$(this).addClass('btn--active');
+$('#'+tab_id).addClass('signup__form--open');
 
+}
+})
+//front end form validation
+$('.btnsubmit').click(function(){
+var selectedId=$(this).parent().attr('id');
+var inputField=$('#'+selectedId).find(".inputField");
+var len=inputField.length;
+for(var i=0;i<len-1;i++){
+if($(inputField[i]).val()==""){
+$(inputField[i]).addClass("input--state-error");
+console.log("the username is black");
+}
+}
+if($(inputField[len-1]).val()==0){
+console.log("no selected");
+$(inputField[len-1]).addClass('input--state-error');
+}
+});
+$('.inputField').click(function(){
+$(this).removeClass('input--state-error');
+});
+//end of front end form validation
 
-	
 });
